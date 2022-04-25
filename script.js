637158c8cfe6bc3926b498e0820ce181f9e9966b
Dani <s.leslie94@gmail.com>
2022-04-25 14:39:22 -0400

    create grid system

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const container = document.querySelector('#container');
+let track = "1fr ";
 
-function createGrid(boxes){
+function createGrid(num){  
+    let divAmount = (num * num);
 
+for (let i = 0; i < num; i++){
+    container.style.display = 'grid';
+    container.style.gridTemplateColumns = track.repeat(num);
+    container.style.gridTemplateRows = track.repeat(num);
+    }
     
+
+for (let j=0; j<divAmount; j++){
+    let box = document.createElement('div');
+    container.append(box);
+    }
 }
-createGrid(10);
+createGrid(20);
+
+// look up different between appendChild and documentFragment, possibly swap out
