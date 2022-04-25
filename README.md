c0c073f314ca104ded0d69a356628ee7a61c75a8
Dani <s.leslie94@gmail.com>
2022-04-25 18:03:44 -0400

    add hover effect and clear button

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # challenges
 - dynamically add boxes to a container based on input
 - change colour of boxes on hover to black
-- add button functionality that resets change without resetting page(?)
+- add button functionality that resets change without resetting page
 - optional: change colour of boxes on hover to rainbow
 
 # areas for improvement
--- script.js
+++ script.js
@@ -1,21 +1,57 @@
 const container = document.querySelector('#container');
 let track = "1fr ";
+let clearButton = document.querySelector('.clear');
+let boxes;
+
+// default grid on page load
 
 function createGrid(num){  
-    let divAmount = (num * num);
+let divAmount = (num * num);
 
 for (let i = 0; i < num; i++){
     container.style.display = 'grid';
     container.style.gridTemplateColumns = track.repeat(num);
     container.style.gridTemplateRows = track.repeat(num);
     }
-    
-
 for (let j=0; j<divAmount; j++){
     let box = document.createElement('div');
+    box.classList.add('box');
     container.append(box);
     }
 }
-createGrid(20);
+createGrid(16);
 
-// look up different between appendChild and documentFragment, possibly swap out
+function changeColour(){
+boxes = document.querySelectorAll(".box");
+//looping through all of the boxes and adding an event listener to each one 
+boxes.forEach(div => {
+    div.addEventListener('mouseover', function(){
+        div.classList.add('colour');
+    });
+});
+}
+changeColour();
+
+
+function eraserMode(){
+    boxes = document.querySelectorAll(".box");
+    boxes.forEach(div => {
+        div.addEventListener('mouseover', function(){
+            div.classList.remove('colour');
+        });
+    });
+}
+function clearGrid(){
+    boxes = document.querySelectorAll(".box");
+    boxes.forEach(div => {
+            div.classList.remove('colour');
+        });
+    };
+
+
+
+
+clearButton.addEventListener('click', clearGrid);
+
+
+
